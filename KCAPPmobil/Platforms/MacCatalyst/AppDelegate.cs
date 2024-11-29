483aa0c0aedc1f60f319761a3971ba4259b8763c
laboratorio <laboratorio@UPEAULA-31703>
2024-11-29 08:04:00 -0500

    Agregar archivos de proyecto.

---
+++ KCAPPmobil/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KCAPPmobil
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
